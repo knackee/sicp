44fb1e806ea323fe93d83f5258e8f76c2566311b
Kyle Smith <mr.kyle.smith@gmail.com>
2013-01-22 16:51:16 -0800

    Updating 1.5 through 1.7

--- ch1.rkt
+++ ch1.rkt
@@ -36,6 +36,54 @@
 ; Exercise 1.4
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
-; Combination - delimited list of expressions denote a procedure. (operator <operands ...>)
+; Combination - delimited list of expressions denote a procedure.
+; (operator <operands ...>)
 ; Compound expression - combination of primitive procedure and numbers.
 ; 
+
+; Exercise 1.5 - see notebook
+(define (p) (p))
+(define (test x y)
+  (if (= x 0) 0 y))
+;(test 0 (p))
+; An applicative-order interpreter will loop forever since (p) is recursive.
+; A normal-order interpreter will return 0 since (p) is never evaluated.
+
+
+(define (square x)
+  (* x x))
+(define (sqrt-iter guess x)
+  (display guess) (newline)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; Exercise 1.6
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+;(define (sqrt-iter guess x)
+;  (new-if (good-enough? guess x)
+;          guess
+;          (sqrt-iter (improve guess x) x)))
+; Since new-if is a standard function definition all of its operands are
+; evaluated with applicative-order. The else-clause within sqrt-iter is a
+; recursive call that will cause the interpreter to loop forever. The special
+; function (if) evaluates the predicate before evaluating either the consequent
+; or alternative. (if) would not cause a loop.
+
+; Exercise 1.7
+; Small numbers: The effectiveness of (sqrt) goes down the closer the radicand
+; is to the tolerance.
+; Large numbers: (sqrt 1e49) loops forever. (improve) returns the same guess.
+; (improve 3.162277660168379e+024 1e49) => 3.162277660168379e+024
+; This is the correct answer but the floating point representation of this
+; number is not within a precision of 0.001.
